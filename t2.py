05a64e8ddee238ba12867e37938a6ae55c5cf016
huangyq89 <94620372+huangyq89@users.noreply.github.com>
2021-12-12 11:29:00 +0800

    Add files via upload

---
+++ t2.py
@@ -0,0 +1,74 @@
+import re
+def mask(splign_result, genome):
+    readfasta(genome)
+    f = open(splign_result, 'r')
+    a = f.readlines()
+    f.close()
+    for line in a:
+        if 'exon' in line:
+            if re.findall(r'\|(\d+) *', line) != []:
+                chromo = re.findall(r'\|(\d+) *', line)[0]
+            elif re.findall(r'\|.*?\|(.*?)\|', line) != []:
+                chromo = re.findall(r'\|.*?\|(.*?)\|', line)[0]
+            else:
+                continue
+            start = re.findall(r'\s+(\d+)', line)[-2]
+            end = re.findall(r'\s+(\d+)', line)[-1]
+            start = int(start)
+            end = int(end)
+            if start > end:
+                start, end = end, start
+            global index
+            for name in index:
+                if re.match(chromo + '\s+', name) is not None:
+                    num = index.index(name)
+                    num = int(num)
+                    new = []
+                    length = end - start + 1
+                    global seq
+                    for s in seq[num]:
+                        new.append(s)
+                    new[start-1:end] = 'N'*length
+                    seq[num] = ''.join(new)
+    sortline(seq)
+    f = open('new.txt', 'w')
+    i = 0
+    while i < len(index):
+        f.writelines('>' + index[i] + '\n')
+        f.writelines(seq[i] + '\n')
+        i += 1
+
+def readfasta(file):
+    f = open(file)
+    lines = f.readlines()
+    f.close()
+    lines.append('>')
+    global index
+    index = []
+    global seq
+    seq = []
+    seqplast = ''
+    for line in lines:
+        if '>' in line:
+            index.append(line.replace('\n', '').replace('>', ''))
+            seq.append(seqplast.replace('\n', ''))
+            seqplast = ''
+        else:
+            seqplast += line.replace('\n', '')
+    index = index[:-1]
+    seq = seq[1:]
+
+def chunkstring(string, length):
+    return (string[0+i:length+i] for i in range(0, len(string), length))
+
+def sortline(seq):
+    for k in seq:
+        new_k = ''
+        for chunk in chunkstring(seq[seq.index(k)], 60):
+            new_k += chunk + '\n'
+        seq[seq.index(k)] = new_k
+splign_result = 'output.splign'
+genome = 'Danio_rerio.GRCz11.dna.alt.fa'
+mask(splign_result, genome)
+
+
