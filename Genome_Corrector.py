eb4f7a474d704c51c1c00a312dd993abd4790274
huangyq89 <94620372+huangyq89@users.noreply.github.com>
2021-12-13 10:49:50 +0800

    Update Genome_Corrector.py

--- Genome_Corrector.py
+++ Genome_Corrector.py
@@ -4,7 +4,8 @@ from add_annotation import *
 import sys
 
 # 接收的参数依次为待更新的参考基因组、待更新的注释文件、用于更新的测序结果、更新后的参考基因组和更新后的注释文件
-mask('splign_result.txt', sys.argv[1], 'mask_result.txt')
+splign(sys.argv[3])
+mask(sys.argv[1])
 add_genome(sys.argv[3], sys.argv[4])
 add_annotation(sys.argv[2], sys.argv[5])
 
